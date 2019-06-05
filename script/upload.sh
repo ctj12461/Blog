63bc7267d1c053e11320471f3c41b0f7863dc14f
Justin Chen <ctj12461@163.com>
2019-06-05 22:09:07 +0800

    Update upload.sh

--- script/upload.sh
+++ script/upload.sh
@@ -2,10 +2,10 @@
 # upload.sh
 cd ..
 git clone git@github.com:ctj12461/photos.git ../photos
-cp -i source/photos/images/* ../photos/images/
+cp -i source/album/images/* ../photos/images/
 cd ../photos
 git add .
 git commit -m "Add photo(s) at `date`"
-git push origin master
+git push -f origin master
 cd ..
 rm -rf photos
