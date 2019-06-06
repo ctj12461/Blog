830e8fc138e8dd1104f27034b8f66986002284db
ctj12461 <1246186412@qq.com>
2019-06-06 21:37:12 +0800

    Update the style of tags.

--- script/sync.sh
+++ script/sync.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 # sync.sh
-#cd ..
-#git init ../BlogSource
-#cp -r script README.md .travis.yml .gitignore scaffolds replace themes source package.json _config.yml ../BlogSource
-#cd ../BlogSource/
-#rm -rf source/photos/images
+
 git add .
 git commit -m "Commit at `date`"
-#git remote add origin git@github.com:ctj12461/Blog.git
-git checkout -b source
 git fetch origin source
 git merge origin/source
 git push origin source
 cd ..
-#rm -rf BlogSource
 
 a=0
 read a
